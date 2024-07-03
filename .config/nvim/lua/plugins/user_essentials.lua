0d295a62189c21ca5efff3da9e372bdbd004786f
guiburon <guillaume.buron@yahoo.fr>
2024-07-03 16:27:29 +0200

    Inital commit

---
+++ .config/nvim/lua/plugins/user_essentials.lua
@@ -0,0 +1,25 @@
+local plugins = {
+  --- line wrapping (toggle with keys: yow) ---
+  {
+    "andrewferrier/wrapping.nvim",
+    config = function()
+      require("wrapping").setup()
+    end,
+  },
+
+  --- toggle comment ---
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      -- add any options here
+    },
+    lazy = false,
+  },
+
+  --- REPL ---
+  -- {
+  --   "Olical/conjure",
+  -- },
+}
+
+return plugins
---
+++ .zshenv
@@ -0,0 +1,7 @@
+ZDOTDIR=$HOME/.config/zsh
+
+export PATH="$HOME/.julia/bin:$PATH"
+export PATH="$HOME/.juliaup/bin:$PATH"
+export PATH="$HOME/nvim/nvim-linux64/bin:$PATH"
+
+# . "$HOME/.cargo/env"
